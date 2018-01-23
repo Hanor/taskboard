ae9bc74c3975a84fa1a1e4115c27cb1847dcbf96
takeuchi <g.takeuchi@gmail.com>
2018-01-23 09:47:28 -0200

    [TOOLS-471] refactoring to replace jira rest client in issue searches and retrieval

--- src/main/java/objective/taskboard/utils/NumberUtils.java
+++ src/main/java/objective/taskboard/utils/NumberUtils.java
@@ -1,15 +1,13 @@
 package objective.taskboard.utils;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
-import static java.util.Arrays.stream;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 public class NumberUtils {
 
